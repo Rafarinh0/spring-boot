a74ca3465c880f13b17307197aea7fc98b34202e
Rafael Marinho <rafael.mgf2001@gmail.com>
2021-06-07 19:03:03 -0300

    show get and dto

--- src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.rafael.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafael.cursomc.domain.Categoria;
+import com.rafael.cursomc.dto.CategoriaDTO;
 import com.rafael.cursomc.services.CategoriaService;
 
 @RestController
@@ -21,6 +24,13 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>>findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO	);
+	}
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
