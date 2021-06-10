48da4aa5b8a4290b0bc65b976b99afc3d5569232
Rafael Marinho <rafael.mgf2001@gmail.com>
2021-06-10 12:46:47 -0300

    customer put get delete methods

--- src/main/java/com/rafael/cursomc/services/ClienteService.java
+++ src/main/java/com/rafael/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.rafael.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rafael.cursomc.domain.Cliente;
+import com.rafael.cursomc.dto.ClienteDTO;
 import com.rafael.cursomc.repositories.ClienteRepository;
+import com.rafael.cursomc.services.exceptions.DataIntegrityException;
 import com.rafael.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,10 +22,46 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
 	public Cliente find(Integer id) {
 		 Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		 "Object not found! Id: " + id + ", Type: " + Cliente.class.getName()));
-		} 
+	} 
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	
+	//eu posso deletar um cliente que tenha endereço, mas nao um que tenha pedidos
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	//private pq eh um metodo auxiliar da classe e n tem pq ficar exposto
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 
 }
